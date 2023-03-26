384fc0cd3a8322e0c7c40b6e434df789fb039d64
Minh Tú <127761066+minhtu122@users.noreply.github.com>
2023-03-26 10:22:30 +0700

    Create day.py

---
+++ day.py
@@ -0,0 +1,17 @@
+day = int(input('Nhập số từ 1-7: '))
+if day==1:
+    print ('Hôm nay là thứ 2')
+elif day==2:
+    print ('Hôm nay là thứ 3')
+elif day==3:
+    print ('Hôm nay là thứ 4')
+elif day==4:
+    print ('Hôm nay là thứ 5')
+elif day==5:
+    print ('Hôm nay là thứ 6')
+elif day==6:
+    print ('Hôm nay là thứ 7')
+elif day==7:
+    print ('Hôm nay là Chủ nhật')
+else:
+    print ('Nhập lại')
